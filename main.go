ba53ff590a656690bbd3c7390f2b3e306ce2fc63
Jose Luis <jocorreamoreno@gmail.com>
2024-04-09 00:03:43 -0500

    Working bot (Initial approach)

--- main.go
+++ main.go
@@ -1,45 +1,108 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"log"
-	"math/rand/v2"
+	"os"
+	"strings"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	//"math/rand/v2"
 )
 
 var (
-	arr = [5]string{"el pepe", "viernes de borde interno", "es correpto", "sale pa la 20", ":)"}
+	bot *tgbotapi.BotAPI
+
+// arr = [5]string{"el pepe", "viernes de borde interno", "es correpto", "sale pa la 20", "mi primer jatriki"}
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("7048395318:AAGUGeQM-wjZymlZujdBQEZZD0EEHj-qB64")
+	var err error
+	bot, err = tgbotapi.NewBotAPI("7048395318:AAGUGeQM-wjZymlZujdBQEZZD0EEHj-qB64")
 	if err != nil {
-		log.Fatalf("Error creating bot: %v", err)
+		log.Panic(err)
 	}
-	// Set the bot to use debug mode (verbose logging).
-	bot.Debug = true
-	log.Printf("Authorized as @%s", bot.Self.UserName)
+
+	// Set this to true to log all interactions with telegram servers
+	bot.Debug = false
+
 	// Set up updates configuration.
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-	// Get updates from the Telegram API.
-	updates, err := bot.GetUpdatesChan(u)
-	if err != nil {
-		log.Fatalf("Error getting updates: %v", err)
-	}
-	// Process incoming messages.
-	for update := range updates {
-		if update.Message == nil { // Ignore any non-Message updates.
-			continue
-		}
-		// Print received message text and sender username.
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		// Respond to the user.
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, arr[rand.IntN(5)])
-		_, err := bot.Send(msg)
-		if err != nil {
-			log.Printf("Error sending message: %v", err)
+	// Create a new cancellable background context. Calling `cancel()` leads to the cancellation of the context
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	// `updates` is a golang channel which receives telegram updates
+	updates := bot.GetUpdatesChan(u)
+
+	// Pass cancellable context to goroutine
+	go receiveUpdates(ctx, updates)
+
+	// Tell the user the bot is online
+	log.Println("Estamo activooooo papi, escribe cualquier mondá")
+
+	// Wait for a newline symbol, then cancel handling updates
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	cancel()
+}
+
+func receiveUpdates(ctx context.Context, updates tgbotapi.UpdatesChannel) {
+	// `for {` means the loop is infinite until we manually stop it
+	for {
+		select {
+		// stop looping if ctx is cancelled
+		case <-ctx.Done():
+			return
+		// receive update from channel and then handle it
+		case update := <-updates:
+			handleUpdate(update)
 		}
 	}
+}
+
+func handleUpdate(update tgbotapi.Update) {
+	if update.Message != nil {
+		handleMessage(update.Message)
+	}
+}
+
+func handleMessage(message *tgbotapi.Message) {
+	user := message.From
+	text := message.Text
+
+	if user == nil {
+		return
+	}
+
+	// Print to console
+	log.Printf("%s wrote %s", user.FirstName, text)
+
+	var err error
+	if strings.HasPrefix(text, "/") {
+		err = handleCommand(message.Chat.ID, text)
+	}
+
+	if err != nil {
+		log.Printf("An error occured: %s", err.Error())
+	}
+}
+
+func handleCommand(chatId int64, command string) error {
+	var err error
+
+	switch command {
+	case "/holi":
+		msg := tgbotapi.NewMessage(chatId, "holi *w*")
+		_, err = bot.Send(msg)
+
+	case "/uwu":
+		msg := tgbotapi.NewMessage(chatId, "uwu")
+		_, err = bot.Send(msg)
+
+	}
+
+	return err
 }
