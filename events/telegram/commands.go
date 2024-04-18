7e5c4bffba93908d59d4c4f7ecd232077878c69a
Brayan Escobar <brayan.escobarm@gmail.com>
2024-04-18 17:08:01 -0500

    implement /track water handler

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -2,7 +2,10 @@ package telegram
 
 import (
 	"activity-tracker/api/telegram"
+	"activity-tracker/storage"
+	"fmt"
 	"strings"
+	"time"
 )
 
 const (
@@ -10,41 +13,47 @@ const (
 	jeje    = "https://static.wikia.nocookie.net/memes-pedia/images/5/5e/Quieres_Pene.jpg/revision/latest/scale-to-width-down/1200?cb=20230507024715&path-prefix=es"
 )
 
-var commandMap = map[string]func(bot *telegram.Bot, chatID int64) error{
-	"/hello":       sendHello,
-	"/help":        sendHelp,
-	"/commands":    sendCommands,
-	"/track":       sendTrackHelp,
-	"/report":      sendReportHelp,
-	"/hatriki":     sendHatriki,
-	"/tengohambre": sendHambre,
-}
+var (
+	goalWaterConsume = 3
+	waterLabel       = "water"
 
-var suffixTrackMap = map[string]func(bot *telegram.Bot, chatID int64) error{
-	"water":      sendTrackWater,
-	"toothbrush": sendTrackTooth,
-	"read":       sendTrackRead,
-	"shower":     sendTrackShower,
-	"sleep":      sendTrackSleep,
-	"gym":        sendTrackGym,
-	"poop":       sendTrackPoop,
-}
+	commandMap = map[string]func(bot *telegram.Bot, chatID int64) error{
+		"/hello":       sendHello,
+		"/help":        sendHelp,
+		"/commands":    sendCommands,
+		"/track":       sendTrackHelp,
+		"/report":      sendReportHelp,
+		"/hatriki":     sendHatriki,
+		"/tengohambre": sendHambre,
+	}
 
-func doCommand(bot *telegram.Bot, chatID int64, command string) error {
+	suffixTrackMap = map[string]func(bot *telegram.Bot, userName string, chatID int64) error{
+		"water":      sendTrackWater,
+		"toothbrush": sendTrackTooth,
+		"read":       sendTrackRead,
+		"shower":     sendTrackShower,
+		"sleep":      sendTrackSleep,
+		"gym":        sendTrackGym,
+		"poop":       sendTrackPoop,
+	}
+)
+
+func doCommand(bot *telegram.Bot, chatID int64, userName string, command string) error {
 	if fn, ok := commandMap[command]; ok {
 		return fn(bot, chatID)
 	}
+
 	// Check if the command starts with "track"
 	if strings.HasPrefix(command, "/track ") {
 		suffix := strings.TrimPrefix(command, "/track ")
-		return handleTrack(bot, chatID, suffix)
+		return handleTrack(bot, chatID, userName, suffix)
 	}
 	return sendUnknownCommand(bot, chatID)
 }
 
-func handleTrack(bot *telegram.Bot, chatID int64, suffix string) error {
+func handleTrack(bot *telegram.Bot, chatID int64, userName, suffix string) error {
 	if fn, ok := suffixTrackMap[suffix]; ok {
-		return fn(bot, chatID)
+		return fn(bot, userName, chatID)
 	}
 	return sendUnknownCommand(bot, chatID)
 }
@@ -69,31 +78,63 @@ func sendTrackHelp(bot *telegram.Bot, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, msgTrack)
 }
 
-func sendTrackWater(bot *telegram.Bot, chatID int64) error {
-	return telegram.SendMessage(bot, chatID, "awa")
+func isGoalCompleted(bot *telegram.Bot, userName string, chatID int64) bool {
+	currentDayWaterActivities, err := storage.GetCurrentDayActivities(userName, waterLabel)
+	if err != nil {
+		telegram.SendMessage(bot, chatID, "tenemos problemas papi"+err.Error())
+
+		return false
+	}
+
+	return len(currentDayWaterActivities) >= goalWaterConsume
 }
 
-func sendTrackTooth(bot *telegram.Bot, chatID int64) error {
+func sendTrackWater(bot *telegram.Bot, userName string, chatID int64) error {
+	isGoalCompleted := isGoalCompleted(bot, userName, chatID)
+	if isGoalCompleted {
+		return telegram.SendMessage(bot, chatID, "ya te tomaste los 3L de awa mi papacho, aprende a tener límites")
+	}
+
+	now := time.Now()
+	formattedNow := now.Format("2006-01-02 15:04:05")
+	id := fmt.Sprintf("%s-%s-%s", formattedNow, userName, waterLabel)
+
+	userActivity := storage.UserActivity{
+		ID:        id,
+		Name:      userName,
+		Activity:  waterLabel,
+		CreatedAt: now,
+	}
+
+	err := storage.Create(userActivity)
+	if err != nil {
+		return telegram.SendMessage(bot, chatID, "algo falló mi fafá: "+err.Error())
+	}
+
+	return telegram.SendMessage(bot, chatID, "se wardó tu tomadita de awa golosito")
+}
+
+func sendTrackTooth(bot *telegram.Bot, userName string, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, "diente")
 }
 
-func sendTrackRead(bot *telegram.Bot, chatID int64) error {
+func sendTrackRead(bot *telegram.Bot, userName string, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, "lectura")
 }
 
-func sendTrackShower(bot *telegram.Bot, chatID int64) error {
+func sendTrackShower(bot *telegram.Bot, userName string, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, "ya era hora olias a obo")
 }
 
-func sendTrackSleep(bot *telegram.Bot, chatID int64) error {
+func sendTrackSleep(bot *telegram.Bot, userName string, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, "zzzzz")
 }
 
-func sendTrackGym(bot *telegram.Bot, chatID int64) error {
+func sendTrackGym(bot *telegram.Bot, userName string, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, "higado al fallo")
 }
 
-func sendTrackPoop(bot *telegram.Bot, chatID int64) error {
+func sendTrackPoop(bot *telegram.Bot, userName string, chatID int64) error {
 	return telegram.SendMessage(bot, chatID, "a ber?")
 }
 
--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -10,7 +10,17 @@ import (
 	"time"
 )
 
-var errMissingUser = errors.New("user object it's empty")
+var (
+	errMissingUser = errors.New("user object it's empty")
+	errInvalidUser = errors.New("user not allowed to use JH Bot")
+
+	allowedUsers = map[string]bool{
+		"BrayanEscobar": true,
+		"mcortazar":     true,
+		"JohanFlorez":   true,
+		"jCorreaM":      true,
+	}
+)
 
 func Fetch(ctx context.Context, bot *telegram.Bot, updates telegram.Channel) (err error) {
 	// `for {` means the loop is infinite until we manually stop it
@@ -47,10 +57,14 @@ func processMessage(bot *telegram.Bot, message *telegram.Message) error {
 		return errMissingUser
 	}
 
+	if _, ok := allowedUsers[user.UserName]; !ok {
+		return errInvalidUser
+	}
+
 	if strings.HasPrefix(text, "/") {
 		// Print to console username,text and date
 		log.Printf("got new command '%s' from '%s at %s", text, user.UserName, date)
-		if err := doCommand(bot, message.Chat.ID, text); err != nil {
+		if err := doCommand(bot, message.Chat.ID, user.UserName, text); err != nil {
 			return fmt.Errorf("can't do command: %w", err)
 		}
 	}
