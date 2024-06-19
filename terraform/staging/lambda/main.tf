11ff7ba3407d3597bc9ac7f70edd89d992a2f177
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-18 22:49:36 -0500

    Add remote backend

--- terraform/staging/lambda/main.tf
+++ terraform/staging/lambda/main.tf
@@ -5,11 +5,18 @@ terraform {
       version = "~> 5.46.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
+      version = "~> 2.4.2"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
+      version = "~> 3.2.2"
     }
+  }
+  backend "s3" {
+    bucket = "terraform-tg-lambda"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
@@ -17,6 +24,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+//Mejorar el manejo de estos secrets luego
 data "aws_secretsmanager_secret" "bot_token" {
   name = "BOT_TOKEN"
 }
