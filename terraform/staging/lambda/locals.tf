4fc0dcf65c1061bd0d7edfd45c72f25dfd0b6f77
Jose_Correa <jocorreamoreno@gmail.com>
2024-09-25 18:08:47 -0500

    Add bot and db keys to local variables

--- terraform/staging/lambda/locals.tf
+++ terraform/staging/lambda/locals.tf
@@ -6,4 +6,7 @@ locals {
   binary_name  = "bootstrap"
   binary_path  = "${path.module}/tf_generated/${local.binary_name}"
   archive_path = "${path.module}/tf_generated/${local.function_name}.zip"
+
+  bot_key = "6363123157:AAEmGId6gLDqSunbaNg9pr_Tjnpb1vQPnRs"
+  db_key  = "activitytracker"
 }
