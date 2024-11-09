8e44c2590b65114b032ea57d60870ae9e4030992
Brayan Escobar <brayan.escobarm@gmail.com>
2024-11-09 15:10:46 -0500

    Add keratine report

--- api/telegram/telegram.go
+++ api/telegram/telegram.go
@@ -12,6 +12,7 @@ func New(token string) (*Bot, error) {
 	if err != nil {
 		return nil, fmt.Errorf("getting bot object failed: %w", err)
 	}
+
 	// Set this to true to log all interactions with telegram servers
 	bot.Debug = false
 	return bot, nil
@@ -19,11 +20,13 @@ func New(token string) (*Bot, error) {
 
 func SendMessage(bot *Bot, chatID int64, text string) error {
 	log.Println(chatID)
+
 	msg := tgbotapi.NewMessage(chatID, text)
 	_, err := bot.Send(msg)
 	if err != nil {
 		return fmt.Errorf("can't send message: %w", err)
 	}
+
 	return nil
 }
 
@@ -33,5 +36,6 @@ func SendPhoto(bot *Bot, chatID int64, url string) error {
 	if err != nil {
 		return fmt.Errorf("can't send photo: %w", err)
 	}
+
 	return nil
 }
