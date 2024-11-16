ced95472c85dad3f07ac2b7863bdf63b70c8756a
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-11-16 18:37:02 -0500

    Add organization access policy and policy attachment

--- terraform/staging/lambda/iam.tf
+++ terraform/staging/lambda/iam.tf
@@ -14,7 +14,33 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
+resource "aws_iam_policy" "organizations_access_policy" {
+  name        = var.organizations_access_policy_name
+  description = "Policy to allow Lambda to access AWS Organizations"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "organizations:DescribeOrganization",
+          "organizations:ListAccounts",
+          "organizations:ListOrganizationalUnitsForParent",
+          "organizations:ListChildren"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_org_policy_attachment" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.organizations_access_policy.arn
 }
