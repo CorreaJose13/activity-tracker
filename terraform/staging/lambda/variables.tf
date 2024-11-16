bcc08386be45d02ea08b440b6392fd52fb8473ea
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-11-16 18:57:11 -0500

    Add new variables to S3, Lambda function and binary naming

--- terraform/staging/lambda/variables.tf
+++ terraform/staging/lambda/variables.tf
@@ -9,3 +9,21 @@ variable "organizations_access_policy_name" {
   type        = string
   default     = "organizations_access_policy"
 }
+
+variable "s3_bucket_name" {
+  description = "Name for the S3 bucket to store Terraform backend"
+  type        = string
+  default     = "terraform-tgbot-lambda"
+}
+
+variable "lambda_function_name" {
+  description = "Name for the Lambda function"
+  type        = string
+  default     = "tg_bot_lambda"
+}
+
+variable "binary_name" {
+  description = "Name for the binary file for Lambda deployment"
+  type        = string
+  default     = "bootstrap"
+}
