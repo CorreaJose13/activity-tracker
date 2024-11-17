d2baa6a44d1b46b616d0b8da56e4137170e622a3
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-11-16 19:00:17 -0500

    Delete unused local variables

--- terraform/staging/lambda/locals.tf
+++ terraform/staging/lambda/locals.tf
@@ -1,11 +1,7 @@
 locals {
-  bucket_name   = "terraform-tgbot-lambda"
-  function_name = "tg_bot_lambda"
-  src_path      = "${path.module}/../../../lambda/main.go"
-
-  binary_name  = "bootstrap"
-  binary_path  = "${path.module}/tf_generated/${local.binary_name}"
-  archive_path = "${path.module}/tf_generated/${local.function_name}.zip"
+  src_path     = "${path.module}/../../../lambda/main.go"
+  binary_path  = "${path.module}/tf_generated/${var.binary_name}"
+  archive_path = "${path.module}/tf_generated/${var.lambda_function_name}.zip"
 
   bot_key = "7048395318:AAGUGeQM-wjZymlZujdBQEZZD0EEHj-qB64"
   db_key  = "activitytracker"
