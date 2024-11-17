c267c0063679f05e7451087332518f0593be6ed7
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-11-17 00:18:34 -0500

    Revert changes made trying to use organization access policy

--- terraform/staging/lambda/iam.tf
+++ terraform/staging/lambda/iam.tf
@@ -14,51 +14,7 @@ resource "aws_iam_role" "lambda_execution_role" {
   })
 }
 
-resource "aws_iam_policy" "restrict_lambda_modifications" {
-  name        = "RestrictLambdaModifications"
-  description = "Restrict updates to Lambda function to the organization"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowOrgToModifyLambda",
-        Effect = "Allow",
-        Action = [
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration",
-          "lambda:AddPermission",
-          "lambda:RemovePermission"
-        ],
-        Resource = aws_lambda_function.lambda_function.arn,
-        Condition = {
-          StringEquals = {
-            "aws:PrincipalOrgID" = "o-dsvxwengs8"
-          }
-        }
-      },
-      {
-        Sid    = "DenyAllOthersFromModifyingLambda",
-        Effect = "Deny",
-        Action = [
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration",
-          "lambda:AddPermission",
-          "lambda:RemovePermission"
-        ],
-        Resource  = aws_lambda_function.lambda_function.arn,
-        Principal = "*"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_modifications_policy_attachment" {
-  role       = aws_iam_role.lambda_execution_role.name
-  policy_arn = aws_iam_policy.restrict_lambda_modifications.arn
 }
