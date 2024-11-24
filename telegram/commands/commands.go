9393d1c606fe2328e915783d97057723f0ac9f20
Brayan Escobar <brayan.escobarm@gmail.com>
2024-11-23 20:09:10 -0500

    Move wishlist from track to commands

--- telegram/commands/commands.go
+++ telegram/commands/commands.go
@@ -2,16 +2,24 @@ package commands
 
 import (
 	"activity-tracker/shared"
+	"activity-tracker/storage"
 	"activity-tracker/telegram/commands/goals"
 	"activity-tracker/telegram/commands/report"
 	"activity-tracker/telegram/commands/track"
+	"errors"
+	"fmt"
 	"strings"
+	"time"
 )
 
 const (
 	hatriki = "https://external-preview.redd.it/jrtz49x5F1cjvDQoFzb0I4cv2dwhA5RDhqaEcBbiXIU.png?format=pjpg&auto=webp&s=3ef741c83f7927eca91cb8ac2d610fd6f010d5b0"
 	jeje    = "https://static.wikia.nocookie.net/memes-pedia/images/5/5e/Quieres_Pene.jpg/revision/latest/scale-to-width-down/1200?cb=20230507024715&path-prefix=es"
 	pinki   = "https://scontent.fclo8-1.fna.fbcdn.net/v/t39.30808-6/438734143_848452533981874_4817032528961290089_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=bd9a62&_nc_ohc=2PwwxOpwaxIQ7kNvgFy3y0R&_nc_zt=23&_nc_ht=scontent.fclo8-1.fna&_nc_gid=A7V0dhgzh2JTTbFFzNtHs3k&oh=00_AYCd9sWEqbIXqz-WtD8NvL0oSChHI8m_oBvz0FfZEofWXA&oe=673849E8"
+
+	expectedWishlistContentParts  = 2
+	invalidURLMessage             = `mandame un link válido bob@ hp, le estoy hablando claro 😡😡😡`
+	invalidWishlistCommandMessage = `no entiendo tu wishlist, usa el formato correcto bob@ hp --> /track wishlist <item> <link>`
 )
 
 var (
@@ -109,6 +117,11 @@ func DoCommand(bot *shared.Bot, chatID int64, userName string, command string) e
 		return handleGoal(bot, chatID, userName, suffix)
 	}
 
+	if strings.HasPrefix(command, "/wishlist ") {
+		suffix := strings.TrimPrefix(command, "/wishlist ")
+		return handleWishlist(bot, chatID, userName, suffix)
+	}
+
 	return sendUnknownCommand(bot, chatID)
 }
 
@@ -189,4 +202,69 @@ func sendPinki(bot *shared.Bot, chatID int64) error {
 	}
 
 	return nil
+}
+
+func handleWishlist(bot *shared.Bot, chatID int64, userName, content string) error {
+	if content == "" {
+		return getWishlist(bot, userName, chatID)
+	}
+
+	contentParts := strings.Split(content, " ")
+	if len(contentParts) != expectedWishlistContentParts {
+		return shared.SendMessage(bot, chatID, invalidWishlistCommandMessage)
+	}
+
+	wishlistItem := contentParts[0]
+	wishlistLink := contentParts[1]
+
+	if wishlistItem == "" || wishlistLink == "" {
+		return shared.SendMessage(bot, chatID, invalidWishlistCommandMessage)
+	}
+
+	if !shared.IsValidURL(wishlistLink) {
+		return shared.SendMessage(bot, chatID, invalidURLMessage)
+	}
+
+	now, err := shared.GetNow()
+	if err != nil {
+		return shared.SendMessage(bot, chatID, err.Error())
+	}
+
+	nowStr := now.Format(time.RFC3339)
+
+	userActivity := shared.UserActivity{
+		ID:        shared.GenerateActivityItemID(now, userName, shared.Wishlist),
+		Name:      userName,
+		Activity:  shared.Wishlist,
+		CreatedAt: nowStr,
+		Content:   content,
+	}
+
+	err = storage.Create(userActivity)
+	if err != nil {
+		return shared.SendMessage(bot, chatID, fmt.Sprintf(shared.ErrSendMessage, err.Error()))
+	}
+
+	msg := fmt.Sprintf("Tu %s se agregó a la wishlist correctamente 🤑", wishlistItem)
+
+	return shared.SendMessage(bot, chatID, msg)
+}
+
+func getWishlist(bot *shared.Bot, userName string, chatID int64) error {
+	wishlistItems, err := storage.GetActivityHistory(userName, shared.Wishlist)
+	if errors.Is(err, storage.ErrNoActivitiesFound) {
+		return shared.SendMessage(bot, chatID, "no tienes nada en tu wishlist, es momento de dejarse llevar por la fiebre del billete 🤑🥵\n\nusate /track wishlist <item> <link>")
+	}
+
+	if err != nil {
+		return shared.SendMessage(bot, chatID, fmt.Sprintf(shared.ErrSendMessage, err.Error()))
+	}
+
+	wishlist := "Weno mi fafá, tu wishlist es la siguiente: \n\n"
+
+	for _, item := range wishlistItems {
+		wishlist += fmt.Sprintf("- %s 🤑\n", item.Content)
+	}
+
+	return shared.SendMessage(bot, chatID, wishlist)
 }
