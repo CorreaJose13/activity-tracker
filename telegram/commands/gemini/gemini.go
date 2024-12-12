70a33a7b322a7adf1ad54ccbb27939878f100225
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-12-12 12:19:31 -0500

    fix pr comments

--- telegram/commands/gemini/gemini.go
+++ telegram/commands/gemini/gemini.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	errNoPrompt = errors.New("En serio metiste un prompt vacío bobo hijueputa? Haz algo bien sub-humano 🙄")
-	forceText   = "responde solo texto"
+	forceText   = ",responde solo texto."
 )
 
 func HandleGemini(bot *shared.Bot, chatID int64, userName, content string) error {
