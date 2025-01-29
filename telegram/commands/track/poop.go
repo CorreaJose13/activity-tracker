4340fd9de7e96ae39f0a5a656b49e31400060db6
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-01-29 18:50:55 -0500

    Add poop tracker and test

--- telegram/commands/track/poop.go
+++ telegram/commands/track/poop.go
@@ -1,8 +1,36 @@
 package track
 
-import "activity-tracker/shared"
+import (
+	"activity-tracker/shared"
+	"activity-tracker/storage"
+	"fmt"
+)
 
-// SendTrackPoop tracks the poop activity
+var (
+	missingPoopContentMessage = "cagaste aire o que malparido????"
+	invalidPoopContentMessage = "al parecer la kk la tienes en el cerebro, manda el numero bien ps 😡"
+	successPoopMessage        = "a ber de q color es? 👀👀"
+)
+
+// SendTrackPoop tracks how many times did u poop
 func SendTrackPoop(client *shared.Client, userName, content string, chatID int64) error {
-	return client.SendMessage(chatID, "a ber?")
+	if content == "" {
+		return client.SendMessage(chatID, missingPoopContentMessage)
+	}
+
+	if !shared.IsValidNumber(content) {
+		return client.SendMessage(chatID, invalidContentMessage)
+	}
+
+	userActivity, err := shared.NewActivity(shared.Poop, userName, content)
+	if err != nil {
+		return client.SendMessage(chatID, err.Error())
+	}
+
+	err = storage.Create(userActivity)
+	if err != nil {
+		return client.SendMessage(chatID, fmt.Sprintf(shared.ErrSendMessage, err.Error()))
+	}
+
+	return client.SendMessage(chatID, successPoopMessage)
 }
