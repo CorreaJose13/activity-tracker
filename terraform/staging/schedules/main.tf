cd307d35826343f51127949ad2391d9a66ce37d7
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-02-18 18:22:07 -0500

    refactor schedules using terraform modules

--- terraform/staging/schedules/main.tf
+++ terraform/staging/schedules/main.tf
@@ -23,5 +23,57 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
+}
+
+locals {
+  src_path     = "${path.module}/../../../functions/schedules/keratine/main.go"
+  binary_path  = "${path.module}/tf_generated/bootstrap"
+  archive_path = "${path.module}/tf_generated/lambda_function.zip"
+}
+
+module "iam_role_lambda_scheduler" {
+  source = "../../modules/iam_role/"
+
+  role_name               = "iam_for_lambda_scheduler"
+  assume_role_identifiers = ["lambda.amazonaws.com"]
+}
+
+module "lambda_function" {
+  source = "../../modules/services/lambda_function/"
+
+  src_path             = local.src_path
+  binary_path          = local.binary_path
+  archive_path         = local.archive_path
+  lambda_function_name = "tg_bot_scheduler_lambda"
+  role_arn             = module.iam_role_lambda_scheduler.role_arn
+  environment_variables = {
+    "BOT_TOKEN" = var.bot_api_token
+  }
+}
+
+module "iam_role_scheduler" {
+  source = "../../modules/iam_role/"
+
+  role_name               = "iam_for_scheduler"
+  assume_role_identifiers = ["scheduler.amazonaws.com"]
+}
+
+module "iam_policy_attachment" {
+  source = "../../modules/iam_policy_attachment/"
+
+  policy_name         = "scheduler_invoke_lambda_policy"
+  action              = "lambda:InvokeFunction"
+  lambda_function_arn = module.lambda_function.lambda_function_arn
+  role_name           = module.iam_role_scheduler.role_name
+}
+
+module "keratine_scheduler" {
+  source = "../../modules/services/scheduler/"
+
+  scheduler_name       = "tg_bot_scheduler"
+  schedule_expression  = "cron(* * * * ?)"
+  role_arn             = module.iam_role_scheduler.role_arn
+  lambda_function_name = module.lambda_function.lambda_function_name
+  message              = "drink-keratine"
 }
