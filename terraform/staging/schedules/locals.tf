90eb52e1c74c55e23101785773b87b5e3108d9ab
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-02-17 23:14:31 -0500

    update locals.tf in schedules

--- terraform/staging/schedules/locals.tf
+++ terraform/staging/schedules/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  src_path     = "${path.module}/../../../schedules/functions/keratine/main.go"
+  src_path     = "${path.module}/../../../../functions/schedules/keratine/main.go"
   binary_path  = "${path.module}/tf_generated/${var.binary_name}"
   archive_path = "${path.module}/tf_generated/${var.scheduler_lambda_function_name}.zip"
 }
